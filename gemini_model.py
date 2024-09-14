9aa897a6e3a4dfb674e20910b049712b6ebcea10
wadissimo <wadissimo@gmail.com>
2024-09-14 10:44:42 +0200

    catching up

--- gemini_model.py
+++ gemini_model.py
@@ -5,8 +5,11 @@ import json
 
 class GeminiModel(Model):
 
-    def __init__(self):
+    def __init__(self, model_name="gemini-pro"):
         super().__init__("Gemini")
+        self.model_name = model_name
+        
+
 
     def call_model(self, prompt):
         # parameters = {
@@ -16,7 +19,9 @@ class GeminiModel(Model):
         #     "top_k": 40,                 
         # }
         #model = TextGenerationModel.from_pretrained("text-bison@001")
-        model = GenerativeModel("gemini-pro")
+        #model = GenerativeModel("gemini-pro")
+        #model = GenerativeModel("gemini-1.5-pro-latest")
+        model = GenerativeModel(self.model_name)
         response = model.generate_content(prompt)
         return response
     
@@ -29,7 +34,8 @@ class GeminiModel(Model):
         return response.text
     
     def call_chat_history(self, message, history):
-        model = GenerativeModel("gemini-pro")
+        #model = GenerativeModel("gemini-pro")
+        model = GenerativeModel(self.model_name)
         # init chat history
         chat_history = []
         for h in history:
